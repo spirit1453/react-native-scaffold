78956f7685513d7e39f685937f7f4533ab4a3362
catch <catchspirit37@gmail.com>
2019-08-27 13:58:11 +0800

    z - Tue Aug 27 2019 13:58:11 GMT+0800 (China Standard Time)

---
+++ bin/publish.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+const semver = require('semver')
+const moment = require('moment')
+const childProcess = require('child_process')
+
+const rootDir = path.resolve(__dirname, '../')
+
+const appJsonPath = path.resolve(rootDir, 'app.json')
+
+let versionBefore = '0.0.1'
+
+if (fs.existsSync(appJsonPath)) {
+    const obj = require(appJsonPath)
+    versionBefore = obj.version
+
+}
+
+
+const newVer = semver.inc(versionBefore, 'patch')
+
+const obj = {
+    version: newVer,
+    time: moment().format('YYYY-MM-DD HH:mm:ss')
+}
+
+fs.writeFileSync(appJsonPath, JSON.stringify(obj))
+
+const cmd = `ys git`
+childProcess.execSync(cmd, {
+    stdio: 'inherit'
+})
